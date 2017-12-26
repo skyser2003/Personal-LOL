b56994b52d85eb8e8b237ffb18d20d82997b5770
Seung Ho Jang <shjang1992@gmail.com>
2017-12-26 23:17:37 +0900

    Test dependency added

--- source/server/main/src/main.cpp
+++ source/server/main/src/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include "FullUrl.h"
 using namespace std;
 
 int main()
 {
-	cout << "Hello World" << endl;
+	RiotApi::FullUrl url(RiotApi::RegionalEndpoint::KR);
+	cout << url.GetUrlFormat() << endl;
+
 	return 0;
 }
