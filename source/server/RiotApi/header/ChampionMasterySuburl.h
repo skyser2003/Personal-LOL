54e1a72dd79fe3c7945d8ba4f1a0d8671e41fe11
장승호(Seung Ho Jang) <shjang1992@gmail.com>
2018-05-16 17:29:05 +0900

    Champion masteries API implemented

--- source/server/RiotApi/header/ChampionMasterySuburl.h
+++ source/server/RiotApi/header/ChampionMasterySuburl.h
@@ -9,23 +9,36 @@ namespace RiotApi
 	class SubUrl<ApiType::CHAMPION_MASTERY_MASTERIES_BY_SUMMONER> : public ISubUrl
 	{
 	public:
+		SubUrl(long summonerId);
+		virtual ~SubUrl();
+
 		std::string GetUrl() const final;
 	private:
+		long summonerId;
 	};
 
 	template <>
 	class SubUrl<ApiType::CHAMPION_MASTERY_MASTERIES_BY_SUMMONER_AND_CHAMPION> : public ISubUrl
 	{
 	public:
+		SubUrl(long summonerId, long championId);
+		virtual ~SubUrl();
+
 		std::string GetUrl() const final;
 	private:
+		long summonerId;
+		long championId;
 	};
 
 	template <>
 	class SubUrl<ApiType::CHAMPION_MASTERY_SCORES_BY_SUMMONER> : public ISubUrl
 	{
 	public:
+		SubUrl(long summonerId);
+		virtual ~SubUrl();
+
 		std::string GetUrl() const final;
 	private:
+		long summonerId;
 	};
 }
--- source/server/RiotApi/src/ChampionMasterySubUrl.cpp
+++ source/server/RiotApi/src/ChampionMasterySubUrl.cpp
@@ -1,2 +1,48 @@
 #include "stdafx.h"
 #include "ChampionMasterySuburl.h"
+
+namespace RiotApi
+{
+	using Summoner = SubUrl<ApiType::CHAMPION_MASTERY_MASTERIES_BY_SUMMONER>;
+	using SummonerAndChampion = SubUrl<ApiType::CHAMPION_MASTERY_MASTERIES_BY_SUMMONER_AND_CHAMPION>;
+	using ScoresBySummoner = SubUrl<ApiType::CHAMPION_MASTERY_SCORES_BY_SUMMONER>;
+
+	// Summoner
+	Summoner::SubUrl(long summonerId) : summonerId(summonerId)
+	{
+
+	}
+
+	Summoner::~SubUrl() = default;
+
+	std::string Summoner::GetUrl() const
+	{
+		return (boost::format("champion-masteries/by-summoner/%L") % summonerId).str();
+	}
+
+	// Summoner and champion
+	SummonerAndChampion::SubUrl(long summonerId, long championId) : summonerId(summonerId), championId(championId)
+	{
+
+	}
+
+	SummonerAndChampion::~SubUrl() = default;
+
+	std::string SummonerAndChampion::GetUrl() const
+	{
+		return (boost::format("champion-masteries/by-summoner/%L/by-champion/%L") % summonerId % championId).str();
+	}
+
+	// Summoner
+	ScoresBySummoner::SubUrl(long summonerId) : summonerId(summonerId)
+	{
+
+	}
+
+	ScoresBySummoner::~SubUrl() = default;
+
+	std::string ScoresBySummoner::GetUrl() const
+	{
+		return (boost::format("scores/by-summoner/%L") % summonerId).str();
+	}
+}
