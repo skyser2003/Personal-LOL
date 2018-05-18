79c9805679c674a9353e3bc077991f5e77a4c1c4
Seung Ho Jang <shjang1992@gmail.com>
2018-05-19 00:49:22 +0900

    Summoner API implemented

--- source/server/RiotApi/header/SummonerSuburl.h
+++ source/server/RiotApi/header/SummonerSuburl.h
@@ -17,4 +17,30 @@ namespace RiotApi
 	private:
 		const long accountId;
 	};
+
+	template <>
+	class SubUrl<ApiType::SUMMONER_SUMMONERS_BY_NAME> : public ISubUrl
+	{
+	public:
+		SubUrl(std::string summonerName);
+		virtual ~SubUrl();
+
+		std::string GetUrl() const final;
+
+	private:
+		const std::string summonerName;
+	};
+
+	template <>
+	class SubUrl<ApiType::SUMMONER_SUMMONERS_BY_SUMMONER_ID> : public ISubUrl
+	{
+	public:
+		SubUrl(long summonerId);
+		virtual ~SubUrl();
+
+		std::string GetUrl() const final;
+
+	private:
+		const long summonerId;
+	};
 }
--- source/server/RiotApi/src/SummonerSuburl.cpp
+++ source/server/RiotApi/src/SummonerSuburl.cpp
@@ -4,8 +4,10 @@
 namespace RiotApi
 {
 	using ByAccount = SubUrl<ApiType::SUMMONER_SUMMONERS_BY_ACCOUNT>;
+	using ByName = SubUrl<ApiType::SUMMONER_SUMMONERS_BY_NAME>;
+	using ById = SubUrl<ApiType::SUMMONER_SUMMONERS_BY_SUMMONER_ID>;
 
-	ByAccount::SubUrl(long accountId) : accountId(accountId)
+	ByAccount::SubUrl(long accountId): accountId(accountId)
 	{
 
 	}
@@ -15,5 +17,29 @@ namespace RiotApi
 	std::string ByAccount::GetUrl() const
 	{
 		return (boost::format("summoner/v3/summoners/by-account/%L") % accountId).str();
+	}
+
+	ByName::SubUrl(std::string summonerName): summonerName(summonerName)
+	{
+
+	}
+
+	ByName::~SubUrl() = default;
+
+	std::string ByName::GetUrl() const
+	{
+		return (boost::format("summoner/v3/summoners/by-name/%L") % summonerName).str();
+	}
+
+	ById::SubUrl(long summonerId): summonerId(summonerId)
+	{
+
+	}
+
+	ById::~SubUrl() = default;
+
+	std::string ById::GetUrl() const
+	{
+		return (boost::format("summoner/v3/summoners/%L") % summonerId).str();
 	}
 }
