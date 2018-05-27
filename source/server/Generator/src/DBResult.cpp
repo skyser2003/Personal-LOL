5f1eb49e24ac10891fa9481513b3507875b0865a
Seung Ho Jang <shjang1992@gmail.com>
2018-05-28 02:44:13 +0900

    Null check

--- source/server/Generator/src/DBResult.cpp
+++ source/server/Generator/src/DBResult.cpp
@@ -1,7 +1,7 @@
 #include "stdafx.h"
 #include "DBResult.h"
 
-DBResult::DBResult(MYSQL_RES* ret) : ret(*ret), count(mysql_num_rows(ret))
+DBResult::DBResult(MYSQL_RES* ret) : ret(*ret), count(ret == nullptr ? 0 : mysql_num_rows(ret))
 {
 	
 }
