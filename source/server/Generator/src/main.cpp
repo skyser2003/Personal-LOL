5fc9e26a423fc6a36b0a941ac9d45d1da8de7659
Seung Ho Jang <shjang1992@gmail.com>
2018-05-27 22:40:45 +0900

    Mysql test connect code added

--- source/server/Generator/src/main.cpp
+++ source/server/Generator/src/main.cpp
@@ -10,6 +10,14 @@ using namespace std;
 using namespace RiotApi;
 using json = nlohmann::json;
 
+struct DBInfo
+{
+	std::string host;
+	std::string db;
+	std::string user;
+	std::string pw;
+};
+
 int main()
 {
 	// API key
@@ -29,6 +37,28 @@ int main()
 		}
 	}();
 
+	// DB info
+	const auto dbInfo = []()
+	{
+		ifstream input("../../../data/db.json");
+		if (input.is_open())
+		{
+			json j;
+			input >> j;
+
+			return DBInfo{
+				j["host"].get<string>(),
+				j["db"].get<string>(),
+				j["user"].get<string>(),
+				j["password"].get<string>()
+			};
+		}
+		else
+		{
+			return DBInfo();
+		}
+	}();
+
 	// API url
 	unique_ptr<ISubUrl> subUrl = make_unique<SubUrl<ApiType::SUMMONER_SUMMONERS_BY_NAME>>("%EC%97%90%ED%8E%A0%ED%83%91");
 	FullUrl url(apiKey, RegionalEndpoint::KR, subUrl);
@@ -41,8 +71,12 @@ int main()
 
 	cout << ret << endl;
 
-	MYSQL config;
-	mysql_change_user(&config, "root", "root", "test");
+	MYSQL* mysql = mysql_init(nullptr);
+	const auto connResult = mysql_real_connect(mysql, dbInfo.host.c_str(), dbInfo.user.c_str(), dbInfo.pw.c_str(), dbInfo.db.c_str(), 3306, nullptr, 0);
+	if (connResult == nullptr)
+	{
+		cout << "DB connect error" << endl;
+	}
 
 	system("pause");
 	return 0;
