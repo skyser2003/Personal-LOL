aebd78b70742adb3c3fbb3684356b2217318d5ed
Seung Ho Jang <shjang1992@gmail.com>
2018-05-27 23:24:24 +0900

    Linux cmake build fix, libmariadb-dev package added for Ubuntu

--- source/server/scripts/cmake.sh
+++ source/server/scripts/cmake.sh
@@ -3,4 +3,4 @@ cd $(dirname ${BASH_SOURCE[0]})
 
 mkdir -p ../build
 cd ../build
-cmake -DCMAKE_CXX_FLAGS=-pthread -DCMAKE_EXE_LINKER_FLAGS="-lnettle -lgnutls -lldap -llber -lz" ../
+cmake -DCMAKE_CXX_FLAGS=-pthread -DCMAKE_EXE_LINKER_FLAGS="-lnettle -lgnutls -lldap -llber -lz -ldl -lmariadb -lssl -lcrypto" ../
