dd2dd641dd1299d2ae13004ca78f3dc92b7b4d94
Seung Ho Jang <shjang1992@gmail.com>
2018-06-14 01:40:17 +0900

    Pre build script added for DataApi project

---
+++ source/server/DataApi/build_script/pre_build.ts
@@ -0,0 +1,9 @@
+﻿import * as fs from "fs";
+import * as path from "path";
+
+import { pbjs, pbts } from "protobufjs/cli";
+
+function generate() {
+    const files = fs.readdirSync(path.join(__dirname, "..", "..", "data", "proto"));
+    const allProtos = files.join(" ");
+}
