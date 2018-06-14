e17f7e93e68ab6bae1fa241b819170810516239f
장승호(Seung Ho Jang) <shjang1992@gmail.com>
2018-06-14 18:25:03 +0900

    tsconfig changed to use es2018 Working build script copied

--- source/server/DataApi/build_script/pre_build.ts
+++ source/server/DataApi/build_script/pre_build.ts
@@ -3,7 +3,45 @@ import * as path from "path";
 
 import { pbjs, pbts } from "protobufjs/cli";
 
-function generate() {
+const protoRegex = /^(.*)\.proto$/;
+
+function generate(filename: string) {
+    return new Promise((resolve, reject) => {
+        const args: string[] = [];
+
+        const match = protoRegex.exec(filename);
+        if (match) {
+            const serviceName = match[2];
+            args.push("-r", serviceName);
+        }
+
+        args.push("-t", "static-module", "-w", "commonjs", "-o", `generated/${filename}.generated.js`);
+        args.push(`./proto/${filename}.proto`);
+
+        pbjs.main(args,
+            (err, output) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    pbts.main(["-o", `generated/${filename}.generated.d.ts`, `generated/${filename}.generated.js`],
+                        (err2, output2) => {
+                            if (err2) {
+                                reject(err);
+                            } else {
+                                resolve();
+                            }
+
+                            return 0;
+                        });
+                }
+
+                return 0;
+            });
+    });
+}
+
+function generateAll() {
     const files = fs.readdirSync(path.join(__dirname, "..", "..", "data", "proto"));
-    const allProtos = files.join(" ");
+
+    const protoFiles = files.filter(filename => protoRegex.test(filename));
 }
