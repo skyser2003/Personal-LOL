59a544ad38ba881ce723290acede167346641990
장승호(Seung Ho Jang) <shjang1992@gmail.com>
2018-06-15 17:35:23 +0900

    Unnecessary cast removed

--- source/server/DataApi/routes/index.ts
+++ source/server/DataApi/routes/index.ts
@@ -8,8 +8,8 @@ import * as packet from "../generated/packet.generated";
 const packetList = grpc.load(path.join(__dirname, "..", "..", "data", "proto", "packet.proto"));
 const TestService = packetList["TestService"] as typeof grpc.Client;
 const client = new TestService("localhost:9999", grpc.credentials.createInsecure());
-
-const testClient = <packet.TestService>(client as object);
+ 
+const testClient = (client as object) as packet.TestService;
 
 const router = express.Router();
 
