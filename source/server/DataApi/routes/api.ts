b56611b1159752b6765027553405979b4359f752
Seung Ho Jang <shjang1992@gmail.com>
2018-06-19 03:32:56 +0900

    api route added

---
+++ source/server/DataApi/routes/api.ts
@@ -0,0 +1,25 @@
+﻿import * as path from "path";
+
+import * as express from "express";
+import * as grpc from "grpc";
+
+import * as packet from "../generated/packet.generated";
+
+const packetList = grpc.load(path.join(__dirname, "..", "..", "data", "proto", "packet.proto"));
+const TestService = packetList["TestService"] as typeof grpc.Client;
+const client = new TestService("localhost:9999", grpc.credentials.createInsecure());
+
+const testClient = (client as object) as packet.TestService;
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    testClient.testSend({ x: 1 },
+        (err, res) => {
+            console.error(err);
+        });
+
+    res.render("index", { title: "Express" });
+});
+
+export default router;
