3fc3e91211c64903043e3f4a28c788c0b9443863
Seung Ho Jang <shjang1992@gmail.com>
2018-06-24 03:05:40 +0900

    Test code removed 2

--- source/server/DataApi/routes/index.ts
+++ source/server/DataApi/routes/index.ts
@@ -1,15 +1,4 @@
-﻿import * as path from "path";
-
-import * as express from "express";
-import * as grpc from "grpc";
-
-import * as packet from "../generated/packet.generated";
-
-const packetList = grpc.load(path.join(__dirname, "..", "..", "data", "proto", "packet.proto"));
-const TestService = packetList["TestService"] as typeof grpc.Client;
-const client = new TestService("localhost:9999", grpc.credentials.createInsecure());
- 
-const testClient = (client as object) as packet.TestService;
+﻿import * as express from "express";
 
 const router = express.Router();
 
