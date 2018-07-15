a17705109111bdb7efbc169b63a3664416fe6e99
Seung Ho Jang <shjang1992@gmail.com>
2018-07-15 17:06:31 +0900

    Register user implemented

--- source/server/Generator/header/DataSaver.h
+++ source/server/Generator/header/DataSaver.h
@@ -9,7 +9,7 @@ class DataSaver
 public:
 	DataSaver(const std::shared_ptr<DBConnection>& conn);
 
-	void RegisterUser(const std::string& summonerName);
+	bool RegisterUser(const std::string& summonerName);
 
 private:
 	std::shared_ptr<DBConnection> conn;
--- source/server/Generator/src/DataSaver.cpp
+++ source/server/Generator/src/DataSaver.cpp
@@ -7,6 +7,9 @@ DataSaver::DataSaver(const std::shared_ptr<DBConnection>& conn) : conn(conn)
 {
 }
 
-void DataSaver::RegisterUser(const std::string& summonerName)
+bool DataSaver::RegisterUser(const std::string& summonerName)
 {
+	const auto result = conn->WriteQuery("INSERT INTO `user` (`summoner_name`) VALUES (%s)", summonerName);
+
+	return result.AffectedRowsCount() != 0;
 }
