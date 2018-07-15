ec6f8d1c0baf1a94f729eb9727efc22e97bb3115
Seung Ho Jang <shjang1992@gmail.com>
2018-07-15 19:07:52 +0900

    Query argument escape implemented

--- source/server/Generator/header/DBConnection.h
+++ source/server/Generator/header/DBConnection.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <boost/format.hpp>
+#include "mysql.h"
 
 #include "DBReadResult.h"
 #include "DBWriteResult.h"
@@ -26,7 +27,7 @@ public:
 	template <typename ...Args>
 	auto ReadQuery(const std::string& query, Args... args)
 	{
-		auto format = (boost::format(query) % ... % args);
+		auto format = (boost::format(query) % ... % Escape(args));
 
 		return ReadQuery(format.str());
 	}
@@ -36,12 +37,32 @@ public:
 	template <typename ...Args>
 	auto WriteQuery(const std::string& query, Args... args)
 	{
-		auto format = (boost::format(query) % ... % args);
+		auto format = (boost::format(query) % ... % Escape(args));
 
 		return WriteQuery(format.str());
 	}
 
 private:
+	template <typename ArgType>
+	std::string Escape(ArgType arg)
+	{
+		const auto strArg = std::to_string(arg);
+		return Escape(strArg);
+	}
+
+	template <>
+	std::string Escape(std::string arg)
+	{
+		auto* dest = new char[arg.length() * 2 + 1];
+
+		mysql_real_escape_string(&conn, dest, arg.c_str(), arg.length());
+
+		auto ret = std::string(dest);
+		delete[] dest;
+
+		return ret;
+	}
+
 	const DBInfo info;
 	MYSQL& conn;
 };
