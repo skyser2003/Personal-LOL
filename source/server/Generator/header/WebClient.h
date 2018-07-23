f998a76fb0df32969f0999e02b0013c529463484
장승호(Seung Ho Jang) <shjang1992@gmail.com>
2018-07-23 20:10:48 +0900

    EncodeURIComponetn and DecodeURIComponent function added

--- source/server/Generator/header/WebClient.h
+++ source/server/Generator/header/WebClient.h
@@ -13,6 +13,9 @@ public:
 	std::string Get(const std::string& url) const;
 	nlohmann::json GetJson(const std::string& url) const;
 
+	std::string EncodeURIComponent(const std::string& uri) const;
+	std::string DecodeURIComponent(const std::string& uri) const;
+
 private:
 	CURL* curl;
 };
--- source/server/Generator/src/WebClient.cpp
+++ source/server/Generator/src/WebClient.cpp
@@ -8,7 +8,7 @@ size_t writeFunction(void *ptr, size_t size, size_t nmemb, std::string* data) {
 
 WebClient::WebClient() : curl(curl_easy_init())
 {
-	
+
 }
 
 WebClient::~WebClient() = default;
@@ -22,7 +22,7 @@ std::string WebClient::Get(const std::string& url) const
 	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &ret);
 
 	auto res = curl_easy_perform(curl);
-	if(res != CURLE_OK)
+	if (res != CURLE_OK)
 	{
 		printf("curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
 	}
@@ -34,5 +34,50 @@ std::string WebClient::Get(const std::string& url) const
 
 nlohmann::json WebClient::GetJson(const std::string& url) const
 {
-	return nlohmann::json(Get(url));
+	return nlohmann::json().parse(Get(url));
+}
+
+std::string WebClient::EncodeURIComponent(const std::string& uri) const
+{
+	const auto* encoded = curl_easy_escape(curl, uri.c_str(), uri.length());
+
+	const auto ret = [encoded]() -> std::string
+	{
+		if (encoded == nullptr)
+		{
+			return "";
+		}
+		else
+		{
+			std::string ret = encoded;
+			delete[] encoded;
+
+			return ret;
+		}
+	}();
+
+	return ret;
+}
+
+std::string WebClient::DecodeURIComponent(const std::string& uri) const
+{
+	int outLength;
+	const auto* decoded = curl_easy_unescape(curl, uri.c_str(), uri.length(), &outLength);
+
+	const auto ret = [decoded]() -> std::string
+	{
+		if (decoded == nullptr)
+		{
+			return "";
+		}
+		else
+		{
+			std::string ret = decoded;
+			delete[] decoded;
+
+			return ret;
+		}
+	}();
+
+	return ret;
 }
