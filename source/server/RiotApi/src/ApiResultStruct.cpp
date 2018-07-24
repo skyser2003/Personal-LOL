ce7c76c0e83264e8c8f120895949923e115e65ce
장승호(Seung Ho Jang) <shjang1992@gmail.com>
2018-07-24 09:22:20 +0900

    Compile error fixed

--- source/server/RiotApi/src/ApiResultStruct.cpp
+++ source/server/RiotApi/src/ApiResultStruct.cpp
@@ -4,7 +4,7 @@
 namespace RiotApi
 {
 	ApiResult<ApiType::SUMMONER_SUMMONERS_BY_NAME>::ApiResult(const nlohmann::json& json) :
-		profileIconId(json["profileIconId"].get<decltype(profileIconId)>)
+		profileIconId(json["profileIconId"].get<decltype(profileIconId)>())
 	{
 
 	}
