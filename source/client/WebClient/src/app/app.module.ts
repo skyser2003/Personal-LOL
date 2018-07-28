8028b5ba83c374f9c576821bbeaebdd982389d70
Seung Ho Jang <shjang1992@gmail.com>
2018-07-28 20:42:19 +0900

    Folder structure changed

--- source/client/WebClient/src/app/app.module.ts
+++ source/client/WebClient/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from "@angular/core";
 
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap"
 
-import { AppComponent } from "./app.component";
+import { AppComponent } from "./src/app.component";
 
 @NgModule({
   declarations: [
