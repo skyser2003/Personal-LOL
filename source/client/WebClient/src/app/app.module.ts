4a48c49c33bf3845aa674c425ab8e8f661a6e8a6
장승호(Seung Ho Jang) <shjang1992@gmail.com>
2018-07-30 14:44:49 +0900

    Index router component added

--- source/client/WebClient/src/app/app.module.ts
+++ source/client/WebClient/src/app/app.module.ts
@@ -1,23 +1,35 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap"
 
 import { AppComponent } from "./component/src/app.component";
 import { HeaderComponent } from "./component/src/header.component";
+import { IndexComponent } from "./component/src/index.component";
 import { LeftMenuComponent } from "./component/src/left-menu.component";
 
+const routes = [
+    {
+        path: "",
+        component: IndexComponent
+    }
+] as Routes;
+
 @NgModule({
-  declarations: [
-      AppComponent,
-      HeaderComponent,
-      LeftMenuComponent
-  ],
-  imports: [
-      BrowserModule,
-      NgbModule.forRoot()
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        IndexComponent,
+        LeftMenuComponent
+    ],
+    imports: [
+        BrowserModule,
+        NgbModule.forRoot(),
+        RouterModule.forRoot(routes)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ source/client/WebClient/src/app/component/src/index.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-index",
+    templateUrl: "../template/index.component.html",
+    styleUrls: ["../css/index.component.css"]
+})
+export class IndexComponent {
+}
