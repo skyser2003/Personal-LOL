d046e4ddbbfec0c4402d40c8b4f8679fedfe482a
장승호(Seung Ho Jang) <shjang1992@gmail.com>
2018-07-30 15:08:52 +0900

    Test script execution path changed

--- source/server/scripts/run_all.bat
+++ source/server/scripts/run_all.bat
@@ -1,6 +1,6 @@
 pushd %~dp0
 
-start "" ..\solution\x64\Debug\Generator.exe
+start "" ..\..\solution\x64\Debug\Generator.exe
 
 cd ../DataApi
 start "" cmd /k "npm run env DEBUG=true	&& node app.js"
