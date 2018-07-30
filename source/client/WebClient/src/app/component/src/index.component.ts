6741c89b5432cf44dc6b3dd6da476b623e34cabb
Seung Ho Jang <shjang1992@gmail.com>
2018-07-31 01:13:02 +0900

    Wrong url fixed

--- source/client/WebClient/src/app/component/src/index.component.ts
+++ source/client/WebClient/src/app/component/src/index.component.ts
@@ -12,7 +12,7 @@ export class IndexComponent {
     }
 
     onSearchSummoner(summonerName: string) {
-        const req = new Request("http://localhost:3000/register", { method: "POST", body: JSON.stringify({ summoner_name: summonerName }) });
+        const req = new Request("http://localhost:3000/api/register", { method: "POST", body: JSON.stringify({ summoner_name: summonerName }) });
         fetch(req)
             .then(res => res.json())
             .then(jsonVal => {
