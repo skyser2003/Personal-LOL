bb83320d51df8c076812a7ed308ea4e0862fcce3
장승호(Seung Ho Jang) <shjang1992@gmail.com>
2018-08-22 17:42:23 +0900

    ng serve added to run script

--- source/server/scripts/run_all.bat
+++ source/server/scripts/run_all.bat
@@ -5,6 +5,9 @@ start "" ..\..\solution\x64\Debug\Generator.exe
 cd ../DataApi
 start "" cmd /k "npm run env DEBUG=true	&& node app.js"
 
+cd ../../client/WebClient
+start "" cmd /k "ng serve"
+
 exit
 
 popd
